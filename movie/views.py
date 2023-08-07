c1b2e32110ae5ef2aa8bbde9a753768b708b641c
Emanuelpa <epatinov@eafit.edu.co>
2023-08-07 09:16:40 -0500

    revision cambio home

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request,'home.html')
-    return render(request, 'home.html',{'name':'Emanuel Patiño Vera'})
+    return render(request, 'home.html',{'name':'Emanuel Patiño Vera.'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
