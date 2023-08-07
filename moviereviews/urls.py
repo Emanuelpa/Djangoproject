d782b4ec927cadfc39a71e97215378972da99230
Emanuelpa <epatinov@eafit.edu.co>
2023-08-07 10:10:02 -0500

    about y nuevo cambio verde

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -20,7 +20,7 @@ from movie import views as movieViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',movieViews.home),
+    path('home',movieViews.home),
     path('about',movieViews.about),
 
 ]
