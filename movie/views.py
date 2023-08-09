7d92e0c53e6a6839ba8707966f643f814b1ee5e6
Emanuelpa <epatinov@eafit.edu.co>
2023-08-09 07:29:07 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,20 +1,20 @@
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Movie
-
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request,'home.html')
-    #return render(request, 'home.html',{'name':'Emanuel Patiño Vera.'})
-    searchTerm = request.GET.get('searchMovie')
+    #return render(request, 'home.html')
+    #return render(request,'home.html',{'name':'Sara Cardona'})
+    searchTerm= request.GET.get('searchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request,'home.html', {'searchTerm':searchTerm,'movies':movies})
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request,'about.html',{'name':'Sara Cardona'},)
